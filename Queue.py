de57ac41577ec3d9249e5bbc03e413648b63ed52
Rustam <43505186+rustam66637@users.noreply.github.com>
2019-04-14 16:09:10 +0500

    Create Queue

---
+++ Queue.py
@@ -0,0 +1,13 @@
+class Queue:
+    def __init__(self):
+        # инициализация хранилища данных
+
+    def enqueue(self, item):
+        # вставка в хвост
+
+    def dequeue(self):
+        # выдача из головы
+        return None # если очередь пустая
+
+    def size(self):
+        return 0 # размер очереди
